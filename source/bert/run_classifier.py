b8d48e9dd001a435263c413f4a5ea302f3ea4108
jackie930 <jackieliu_cornell@163.com>
2020-10-19 17:32:16 +0800

    update metric

--- source/bert/run_classifier.py
+++ source/bert/run_classifier.py
@@ -785,9 +785,15 @@ def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
                 accuracy = tf.metrics.accuracy(
                     labels=label_ids, predictions=predictions, weights=is_real_example)
                 loss = tf.metrics.mean(values=per_example_loss, weights=is_real_example)
+                auc = tf.metrics.auc(labels=label_ids, predictions=predictions, weights=is_real_example)
+                precision = tf.metrics.precision(labels=label_ids, predictions=predictions, weights=is_real_example)
+                recall = tf.metrics.recall(labels=label_ids, predictions=predictions, weights=is_real_example)
                 return {
                     "eval_accuracy": accuracy,
                     "eval_loss": loss,
+                    "auc": auc,
+                    "precision" : precision,
+                    "recall": recall
                 }
 
             eval_metrics = (metric_fn,
