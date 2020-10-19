b91c67f5f1f56b4af382c3c4bf953678c1be1b8a
jackie930 <jackieliu_cornell@163.com>
2020-10-19 13:00:14 +0800

    update class for gt

--- source/bert/run_classifier.py
+++ source/bert/run_classifier.py
@@ -422,6 +422,45 @@ class ChnsenticorpProcessor(DataProcessor):
                 InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
         return examples
 
+class GTProcessor(DataProcessor):
+    """Processor for the Chnsenticorp data set."""
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+
+    def get_test_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+
+    def get_labels(self):
+        """See base class."""
+        return ["1", "0", "-1"]
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            # Only the test set has a header
+            if i == 0:
+                continue
+            guid = "%s-%s" % (set_type, i)
+            if set_type == "test":
+                text_a = tokenization.convert_to_unicode(line[1])
+                label = "0"
+            else:
+                text_a = tokenization.convert_to_unicode(line[1])
+                label = tokenization.convert_to_unicode(line[0])
+            examples.append(
+                InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+        return examples
 
 def convert_single_example(ex_index, example, label_list, max_seq_length,
                            tokenizer):
@@ -849,6 +888,7 @@ def main(_):
         "mrpc": MrpcProcessor,
         "xnli": XnliProcessor,
         'chnsenticorp': ChnsenticorpProcessor,
+        'gt': GTProcessor
     }
 
     tokenization.validate_case_matches_checkpoint(FLAGS.do_lower_case,
---
+++ source/shell_scripts/run_all.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+source activate tensorflow_p36
+export BERT_BASE_DIR=./bert/pretrain_model/chinese_L-12_H-768_A-12
+
+python ./bert/run_classifier.py \
+  --data_dir='../../data/label_f_stock_v2_all' \
+  --task_name='gt' \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --output_dir=./output/ \
+  --do_train=true \
+  --do_eval=true \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --max_seq_length=200 \
+  --train_batch_size=16 \
+  --learning_rate=5e-5\
+  --num_train_epochs=1.0\
+  --save_checkpoints_steps=100\
+  --weight_list='1,1,1'
+
