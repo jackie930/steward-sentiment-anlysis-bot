9bb608e5af1c71f5a635ba417d5d02cdb77f1ae8
jackie930 <jackieliu_cornell@163.com>
2020-10-28 12:14:39 +0800

    update

--- source/bert/preprocess_gt.py
+++ source/bert/preprocess_gt.py
@@ -17,12 +17,9 @@ def process_data_pred(pred_folder,save_path):
             print (i)
             data = pd.read_csv(os.path.join(pred_folder,i))
             ls.append(data)
-    print(ls)
     df=ls[0]
     for i in range(1,len(ls)):
         df=df.append(ls[i])
-    print ("<<<<<<df shape", df.shape)
-    print (df.columns)
     df.to_csv(os.path.join(save_path,'org_pred.csv'),index=False)
     # save as defined
     df1 = df[['label','input']]
@@ -30,49 +27,50 @@ def process_data_pred(pred_folder,save_path):
     df1.to_csv(os.path.join(save_path,'dev.tsv'),sep='\t',index=False)
     df1.to_csv(os.path.join(save_path,'test.tsv'),sep='\t',index=False)
     print ("<<<< save test result")
-    print (df1.head())
+    print ("<<<<<<df shape",df1.shape)
 
 
 #process train
 def process_data_train(input_train_file,save_path):
     """Pred folder contains info to be test on"""
     df = pd.read_csv(input_train_file)
-    print ("<<<<<<df shape", df.shape)
-    print (df.columns)
     # save as defined
     df1 = df[['label','text_a']]
     print (df1.label.unique())
     df1.columns=['label','text_a']
     df1.to_csv(os.path.join(save_path,'train.tsv'),sep='\t',index=False)
     print ("<<<< save train result")
-    print (df1.head())
+    print ("<<<<<<df shape",df1.shape)
 
 def main(pred_folder,input_train_file,save_path):
     process_data_pred(pred_folder,save_path)
     process_data_train(input_train_file,save_path)
 
 if __name__ == "__main__":
-    #exp1
-    pred_folder = '/Users/liujunyi/Desktop/spottag/cathysite/data-2/aws_results/hind'
-    input_train_file = '/Users/liujunyi/Desktop/spottag/cathysite/data-2/exp_h.csv'
-    save_path =  '../output'
+    #exp1, hind, imbalanced
+    print ("<<<<EXP 1")
+    pred_folder = '/home/ec2-user/SageMaker/gt/experiment_org/aws_results_v2/hind'
+    input_train_file = '/home/ec2-user/SageMaker/gt/experiment_org/exp_v2/exp_h_v2.csv'
+    save_path =  '/home/ec2-user/SageMaker/gt/experiments/exp1'
     main(pred_folder,input_train_file,save_path)
 
-    #exp2
-    pred_folder = '/Users/liujunyi/Desktop/spottag/cathysite/data-2/aws_results/hind'
-    input_train_file = '/Users/liujunyi/Desktop/spottag/cathysite/data-2/exp_h.csv'
-    save_path =  '../output'
+    #exp2, hind, enhanced
+    print ("<<<<EXP 2")
+    pred_folder = '/home/ec2-user/SageMaker/gt/experiment_org/aws_results_v2/hind'
+    input_train_file = '/home/ec2-user/SageMaker/gt/experiment_org/exp_v2/exp_h_enhance_v2.csv'
+    save_path =  '/home/ec2-user/SageMaker/gt/experiments/exp2'
     main(pred_folder,input_train_file,save_path)
 
-    #exp3
-    pred_folder = '/Users/liujunyi/Desktop/spottag/cathysite/data-2/aws_results/hind'
-    input_train_file = '/Users/liujunyi/Desktop/spottag/cathysite/data-2/exp_h.csv'
-    save_path =  '../output'
+    #exp3 fore, imbalanced
+    print ("<<<<EXP 3")
+    pred_folder = '/home/ec2-user/SageMaker/gt/experiment_org/aws_results_v2/fore'
+    input_train_file = '/home/ec2-user/SageMaker/gt/experiment_org/exp_v2/exp_f_v2.csv'
+    save_path =  '/home/ec2-user/SageMaker/gt/experiments/exp3'
     main(pred_folder,input_train_file,save_path)
 
-    #exp4
-    pred_folder = '/Users/liujunyi/Desktop/spottag/cathysite/data-2/aws_results/hind'
-    input_train_file = '/Users/liujunyi/Desktop/spottag/cathysite/data-2/exp_h.csv'
-    save_path =  '../output'
+    #exp4 fore, enhanced
+    print ("<<<<EXP 4")
+    pred_folder = '/home/ec2-user/SageMaker/gt/experiment_org/aws_results_v2/fore'
+    input_train_file = '/home/ec2-user/SageMaker/gt/experiment_org/exp_v2/exp_f_enhance_v2.csv'
+    save_path =  '/home/ec2-user/SageMaker/gt/experiments/exp4'
     main(pred_folder,input_train_file,save_path)
-
--- source/gt/gen_data_exp1.sh
+++ source/gt/gen_data_exp1.sh
@@ -2,6 +2,12 @@
 # execute the task
 echo "task starts..."
 
+#download data from s3
+mkdir ../gt_data
+aws s3 cp s3://cathysite-ecv ../gt_data --recursive
+unzip ../gt_data/*.zip
+
+#run preprocess
 python ../bert/preprocess_gt.py
 
 echo "task is done..."
